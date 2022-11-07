43ce91d0cd1f506ad428c22cfd32e3153dca3381
Aurélien Montmejat <aurelien.montmejat@gmail.com>
2022-11-07 01:11:27 +0100

    Add ops and children to Value

--- src/engine.rs
+++ src/engine.rs
@@ -1,22 +1,47 @@
 use std::fmt;
-use std::ops;
+use std::ops::{Add, Mul};
+use std::vec::Vec;
+
+enum Ops {
+    New,
+    Add,
+    Mul,
+}
 
 pub struct Value {
     data: f32,
+    ops: Ops,
+    children: Vec<Value>,
 }
 
 impl Value {
     pub fn new(value: f32) -> Value {
-        Value { data: value }
+        Value {
+            data: value,
+            ops: Ops::New,
+            children: Vec::new(),
+        }
     }
 }
 
-impl ops::Add<Value> for Value {
-    type Output = Value;
-
-    fn add(self, _right_value: Value) -> Value {
+impl Add for Value {
+    type Output = Self;
+    fn add(self, right_value: Self) -> Self {
         Value {
-            data: self.data + _right_value.data,
+            data: self.data + right_value.data,
+            ops: Ops::Add,
+            children: vec![self, right_value],
+        }
+    }
+}
+
+impl Mul for Value {
+    type Output = Self;
+    fn mul(self, right_value: Self) -> Self {
+        Value {
+            data: self.data * right_value.data,
+            ops: Ops::Mul,
+            children: vec![self, right_value],
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,7 @@ fn main() {
     let b = engine::Value::new(5.0);
     println!("b: {}", b);
 
-    println!("a + b = {}", a + b);
+    let c = engine::Value::new(-1.3);
+
+    println!("(a + b) * c = {}", (a + b) * c);
 }
