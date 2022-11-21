5fe231575e637ed8c50bf054bfd03c4bdc7578c9
Aurélien Montmejat <aurelien.montmejat@gmail.com>
2022-11-21 01:20:52 +0100

    Fix back propagation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 mod scalar;
 
 fn main() {
-    // Basic neuron
+    let a = scalar::Scalar::new(10.0);
+    let b = scalar::Scalar::new(20.0);
+
+    let c = &a + &b;
+    let d = &a * &b;
+
+    let mut e = c + d;
+
+    e.backward();
+    println!("");
+
+    // Basic neuron example
     let x1 = scalar::Scalar::new(2.0);
     let x2 = scalar::Scalar::new(0.0);
-    
+
     let w1 = scalar::Scalar::new(-3.0);
     let w2 = scalar::Scalar::new(1.0);
 
@@ -13,9 +24,7 @@ fn main() {
     let sum = (x1 * w1) + (x2 * w2) + b;
     let mut y = sum.tanh();
 
-    println!("Output: {}", y);
+    println!("y: {}", y);
 
     y.backward();
-
-    println!("(a + b) * c = {}", (a + b) * c);
 }
--- src/scalar.rs
+++ src/scalar.rs
@@ -1,73 +1,109 @@
-use std::fmt;
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::fmt;
 use std::ops::{Add, Mul};
+use std::rc::Rc;
 
-type BackwardFn = Option<fn(scalar: &mut Scalar)>; 
+#[derive(Debug, Clone)]
+enum Ops {
+    Add,
+    Mul,
+    Tanh,
+    Nil,
+}
 
-pub struct Scalar {
+#[derive(Clone)]
+pub struct Scalar(Rc<RefCell<ScalarData>>);
+
+#[derive(Clone)]
+pub struct ScalarData {
     data: f32,
     grad: f32,
-    left_child: Option<Rc<RefCell<Scalar>>>,
-    right_child: Option<Rc<RefCell<Scalar>>>,
-    _backward: BackwardFn,
+    left_child: Option<Scalar>,
+    right_child: Option<Scalar>,
+    ops: Ops,
+}
+
+fn compute_grad(scalar: &mut ScalarData) {
+    match scalar.ops {
+        Ops::Add => {
+            if scalar.left_child.is_none() || scalar.right_child.is_none() {
+                return;
+            }
+
+            scalar.left_child.as_ref().unwrap().0.borrow_mut().grad += scalar.grad;
+            scalar.right_child.as_ref().unwrap().0.borrow_mut().grad += scalar.grad;
+        }
+        Ops::Mul => {
+            if scalar.left_child.is_none() || scalar.right_child.is_none() {
+                return;
+            }
+
+            let left_child = &mut scalar.left_child.as_ref().unwrap().0.borrow_mut();
+            let right_child = &mut scalar.right_child.as_ref().unwrap().0.borrow_mut();
+
+            left_child.grad += right_child.data * scalar.grad;
+            right_child.grad += left_child.data * scalar.grad;
+        }
+        Ops::Tanh => {
+            if scalar.left_child.is_none() {
+                return;
+            }
+
+            let left_child = &mut scalar.left_child.as_ref().unwrap().0.borrow_mut();
+            left_child.grad += (1.0 - scalar.data * scalar.data) * scalar.grad;
+        }
+        Ops::Nil => {}
+    }
 }
 
 impl Scalar {
     pub fn new(data: f32) -> Scalar {
-        Scalar {
+        Scalar(Rc::new(RefCell::new(ScalarData {
             data,
             grad: 0.0,
             left_child: None,
             right_child: None,
-            _backward: None,
-        }
+            ops: Ops::Nil,
+        })))
     }
 
     pub fn tanh(self) -> Scalar {
-        Scalar {
-            data: self.data.tanh(),
+        let data = self.0.borrow().data.tanh();
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data,
             grad: 0.0,
-            left_child: Some(Rc::new(RefCell::new(self))),
+            left_child: Some(self),
             right_child: None,
-            _backward: Some(|scalar: &mut Scalar| {
-                let child;
-                match &scalar.left_child {
-                    Some(left_child) => child = left_child,
-                    None => return,
-                }
-                child.borrow_mut().grad = (1.0 - scalar.data * scalar.data) * scalar.grad;
-            }),
-        }
+            ops: Ops::Tanh,
+        })))
     }
 
     pub fn backward(&mut self) {
-        self.grad = 1.0;
-        self._backward.unwrap()(self);  
+        let mut scalar = self.0.borrow_mut();
+        scalar.grad = 1.0;
 
-        let child = self.left_child.as_ref();
-        println!("{}", child.unwrap().borrow().grad);
+        compute_grad(&mut scalar);
+        println!("{}", scalar);
 
-        let mut nodes = vec![self.left_child.clone()];
+        let mut nodes = Vec::new();
+        if !scalar.left_child.is_none() {
+            nodes.push(scalar.left_child.clone().unwrap());
+        }
+        if !scalar.right_child.is_none() {
+            nodes.push(scalar.right_child.clone().unwrap());
+        }
+
         while nodes.len() > 0 {
-            let child;
-            match nodes.pop() {
-                Some(popped_child) => child = popped_child.unwrap(),
-                None => continue,
-            }
+            let child = nodes.pop().unwrap();
 
-            let mut scalar = child.borrow_mut();
-            println!("{}", scalar);
-            match scalar._backward {
-                Some(_) => scalar._backward.unwrap()(&mut scalar),
-                None => {},
-            }
+            compute_grad(&mut child.0.borrow_mut());
+            println!("{}", child);
 
-            if !scalar.left_child.is_none() {
-                nodes.push(scalar.left_child.clone());
+            if !child.0.borrow().left_child.is_none() {
+                nodes.push(child.0.borrow().left_child.clone().unwrap());
             }
-            if !scalar.right_child.is_none() {
-                nodes.push(scalar.right_child.clone());
+            if !child.0.borrow().right_child.is_none() {
+                nodes.push(child.0.borrow().right_child.clone().unwrap());
             }
         }
     }
@@ -76,61 +112,76 @@ impl Scalar {
 impl Add for Scalar {
     type Output = Self;
     fn add(self, other: Self) -> Self {
-        Scalar {
-            data: self.data + other.data,
-            grad: 1.0,
-            left_child: Some(Rc::new(RefCell::new(self))),
-            right_child: Some(Rc::new(RefCell::new(other))),
-            _backward: Some(|scalar: &mut Scalar| {
-                let lchild;
-                match &scalar.left_child {
-                    Some(left_child) => lchild = left_child,
-                    None => return,
-                }
+        let data = self.0.borrow().data + other.0.borrow().data;
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data,
+            grad: 0.0,
+            left_child: Some(self),
+            right_child: Some(other),
+            ops: Ops::Add,
+        })))
+    }
+}
 
-                let rchild;
-                match &scalar.right_child {
-                    Some(right_child) => rchild = right_child,
-                    None => return,
-                }
+impl<'a, 'b> Add<&'b Scalar> for &'a Scalar {
+    type Output = Scalar;
 
-                lchild.borrow_mut().grad = scalar.grad;
-                rchild.borrow_mut().grad = scalar.grad;
-            }),
-        }
+    fn add(self, other: &'b Scalar) -> Scalar {
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data: self.0.borrow().data + other.0.borrow().data,
+            grad: 0.0,
+            left_child: Some(Scalar(Rc::clone(&self.0))),
+            right_child: Some(Scalar(Rc::clone(&other.0))),
+            ops: Ops::Add,
+        })))
     }
 }
 
 impl Mul for Scalar {
-   type Output = Self;
-   fn mul(self, other: Self) -> Self {
-        Scalar {
-            data: self.data * other.data,
-            grad: 1.0,
-            left_child: Some(Rc::new(RefCell::new(self))),
-            right_child: Some(Rc::new(RefCell::new(other))),
-            _backward: Some(|scalar: &mut Scalar| {
-                let lchild;
-                match &scalar.left_child {
-                    Some(left_child) => lchild = left_child,
-                    None => return,
-                }
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        let data = self.0.borrow().data * other.0.borrow().data;
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data,
+            grad: 0.0,
+            left_child: Some(self),
+            right_child: Some(other),
+            ops: Ops::Mul,
+        })))
+    }
+}
 
-                let rchild;
-                match &scalar.right_child {
-                    Some(right_child) => rchild = right_child,
-                    None => return,
-                }
+impl<'a, 'b> Mul<&'b Scalar> for &'a Scalar {
+    type Output = Scalar;
 
-                lchild.borrow_mut().grad = rchild.borrow().data * scalar.grad;
-                rchild.borrow_mut().grad = lchild.borrow().data * scalar.grad;
-            }),
-        }
-   }
+    fn mul(self, other: &'b Scalar) -> Scalar {
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data: self.0.borrow().data * other.0.borrow().data,
+            grad: 0.0,
+            left_child: Some(Scalar(Rc::clone(&self.0))),
+            right_child: Some(Scalar(Rc::clone(&other.0))),
+            ops: Ops::Mul,
+        })))
+    }
 }
 
 impl fmt::Display for Scalar {
-   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-       write!(f, "Scalar(data={}, grad={}, _backward_is_none={})", self.data, self.grad, self._backward.is_none())
-   }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let scalar = self.0.borrow();
+        write!(
+            f,
+            "Scalar(data={}, grad={}, ops={:?})",
+            scalar.data, scalar.grad, scalar.ops,
+        )
+    }
+}
+
+impl fmt::Display for ScalarData {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "Scalar(data={}, grad={}, ops={:?})",
+            self.data, self.grad, self.ops,
+        )
+    }
 }
