2621a54efac4a4288832ef4fa792763803c770e7
Aurélien Montmejat <aurelien.montmejat@gmail.com>
2022-11-29 00:22:00 +0100

    Add division, exponential and power ops

--- src/scalar.rs
+++ src/scalar.rs
@@ -1,13 +1,16 @@
 use std::cell::RefCell;
 use std::fmt;
-use std::ops::{Add, Mul};
+use std::ops::{Add, Div, Mul};
 use std::rc::Rc;
 
 #[derive(Debug, Clone)]
 enum Ops {
     Add,
     Mul,
+    Div,
     Tanh,
+    Exp,
+    Pow,
     Nil,
 }
 
@@ -44,6 +47,19 @@ fn compute_grad(scalar: &mut ScalarData) {
             left_child.grad += right_child.data * scalar.grad;
             right_child.grad += left_child.data * scalar.grad;
         }
+        Ops::Div => {
+            if scalar.left_child.is_none() || scalar.right_child.is_none() {
+                return;
+            }
+
+            let left_child = &mut scalar.left_child.as_ref().unwrap().0.borrow_mut();
+            let right_child = &mut scalar.right_child.as_ref().unwrap().0.borrow_mut();
+
+            left_child.grad += scalar.grad / right_child.data;
+            right_child.grad += -(scalar.grad * left_child.data) / (right_child.data.powi(2));
+            // left_child.grad += -(1.0 / right_child.data.powi(2)) * scalar.grad;
+            // right_child.grad += -(1.0 / left_child.data.powi(2)) * scalar.grad;
+        }
         Ops::Tanh => {
             if scalar.left_child.is_none() {
                 return;
@@ -51,6 +67,27 @@ fn compute_grad(scalar: &mut ScalarData) {
 
             let left_child = &mut scalar.left_child.as_ref().unwrap().0.borrow_mut();
             left_child.grad += (1.0 - scalar.data * scalar.data) * scalar.grad;
+        }
+        Ops::Exp => {
+            if scalar.left_child.is_none() {
+                return;
+            }
+
+            let left_child = &mut scalar.left_child.as_ref().unwrap().0.borrow_mut();
+            left_child.grad += scalar.data * scalar.grad;
+        }
+        Ops::Pow => {
+            if scalar.left_child.is_none() || scalar.right_child.is_none() {
+                return;
+            }
+
+            let left_child = &mut scalar.left_child.as_ref().unwrap().0.borrow_mut();
+            let right_child = &mut scalar.right_child.as_ref().unwrap().0.borrow_mut();
+
+            left_child.grad +=
+                right_child.data * left_child.data.powf(right_child.data - 1.0) * scalar.grad;
+            right_child.grad +=
+                left_child.data * right_child.data.powf(left_child.data - 1.0) * scalar.grad;
         }
         Ops::Nil => {}
     }
@@ -75,6 +112,37 @@ impl Scalar {
             left_child: Some(self),
             right_child: None,
             ops: Ops::Tanh,
+            visited_in_backprop: false,
+        })))
+    }
+
+    pub fn exp(&self) -> Scalar {
+        let data = self.0.borrow().data.exp();
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data,
+            grad: 0.0,
+            left_child: Some(self.clone()),
+            right_child: None,
+            ops: Ops::Exp,
+        })))
+    }
+
+    pub fn powf(self, power: f32) -> Scalar {
+        let data = self.0.borrow().data.powf(power);
+        let other = Scalar(Rc::new(RefCell::new(ScalarData {
+            data: power,
+            grad: 0.0,
+            left_child: None,
+            right_child: None,
+            ops: Ops::Nil,
+        })));
+
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data,
+            grad: 0.0,
+            left_child: Some(self),
+            right_child: Some(other),
+            ops: Ops::Pow,
         })))
     }
 
@@ -88,10 +156,10 @@ impl Scalar {
         let mut nodes = Vec::new();
         if !scalar.left_child.is_none() {
             nodes.push(scalar.left_child.clone().unwrap());
-        }
+                }
         if !scalar.right_child.is_none() {
             nodes.push(scalar.right_child.clone().unwrap());
-        }
+                }
 
         while nodes.len() > 0 {
             let child = nodes.pop().unwrap();
@@ -104,7 +172,7 @@ impl Scalar {
             }
             if !child.0.borrow().right_child.is_none() {
                 nodes.push(child.0.borrow().right_child.clone().unwrap());
-            }
+        }
         }
     }
 }
@@ -161,6 +229,35 @@ impl<'a, 'b> Mul<&'b Scalar> for &'a Scalar {
             left_child: Some(Scalar(Rc::clone(&self.0))),
             right_child: Some(Scalar(Rc::clone(&other.0))),
             ops: Ops::Mul,
+            visited_in_backprop: false,
+        })))
+    }
+}
+
+impl Div for Scalar {
+    type Output = Self;
+    fn div(self, other: Self) -> Self {
+        let data = self.0.borrow().data / other.0.borrow().data;
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data,
+            grad: 0.0,
+            left_child: Some(self),
+            right_child: Some(other),
+            ops: Ops::Div,
+        })))
+    }
+}
+
+impl<'a, 'b> Div<&'b Scalar> for &'a Scalar {
+    type Output = Scalar;
+
+    fn div(self, other: &'b Scalar) -> Scalar {
+        Scalar(Rc::new(RefCell::new(ScalarData {
+            data: self.0.borrow().data / other.0.borrow().data,
+            grad: 0.0,
+            left_child: Some(Scalar(Rc::clone(&self.0))),
+            right_child: Some(Scalar(Rc::clone(&other.0))),
+            ops: Ops::Div,
         })))
     }
 }
