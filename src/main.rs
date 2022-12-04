5b07d835d97ad9fe11f04d0621f881c3b80b6b95
Aurélien Montmejat <aurelien.montmejat@gmail.com>
2022-12-05 00:33:47 +0100

    Update main example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+mod nn;
 mod scalar;
+
 #[macro_use]
 extern crate is_close;
 
 fn main() {
+    // Autograd example
     let x1 = scalar::Scalar::new(2.0);
     let x2 = scalar::Scalar::new(0.0);
 
@@ -17,10 +20,56 @@ fn main() {
 
     output.backward();
 
-    assert!(is_close!(x1.grad(), -1.5, abs_tol=1e-5));
-    assert!(is_close!(x2.grad(), 0.5, abs_tol=1e-5));
-    assert!(is_close!(w1.grad(), 1.0, abs_tol=1e-5));
-    assert!(is_close!(w2.grad(), 0.0, abs_tol=1e-5));
+    assert!(is_close!(x1.grad(), -1.5, abs_tol = 1e-5));
+    assert!(is_close!(x2.grad(), 0.5, abs_tol = 1e-5));
+    assert!(is_close!(w1.grad(), 1.0, abs_tol = 1e-5));
+    assert!(is_close!(w2.grad(), 0.0, abs_tol = 1e-5));
 
-    println!("output: {}", output);
+    // Tiny neural network example
+    let x = vec![
+        scalar::svec![2.0, 3.0, -1.0],
+        scalar::svec![3.0, -1.0, 0.5],
+        scalar::svec![0.5, 1.0, 1.0],
+        scalar::svec![1.0, 1.0, -1.0],
+    ];
+    let y = scalar::svec![1.0, -1.0, -1.0, 1.0];
+
+    let model = nn::Sequential::new(vec![
+        nn::Linear::new(3, 4),
+        nn::Tanh::new(),
+        nn::Linear::new(4, 4),
+        nn::Tanh::new(),
+        nn::Linear::new(4, 1),
+        nn::Tanh::new(),
+    ]);
+
+    let iterations = 100;
+    for i in 0..iterations {
+        let mut predictions = Vec::new();
+        for a in &x {
+            let out = model.forward(a);
+            predictions.push(out[0].clone());
+        }
+
+        let mut loss = scalar::Scalar::new(0.0);
+        for i in 0..y.len() {
+            loss = loss + (y[i].clone() - predictions[i].clone()).powf(2.0);
+        }
+
+        model.zero_grad();
+        loss.backward();
+
+        // println!("{:?}", model.parameters());
+        for param in model.parameters() {
+            param.set_data(param.data() - 0.1 * param.grad());
+        }
+
+        println!("{}/{} | loss: {}", i, iterations, loss.data());
+    }
+
+    for i in 0..x.len() {
+        let pred = model.forward(&x[i]);
+        println!("Pred: {} | Ground truth: {}", pred[0].data(), y[i].data());
+        assert!(is_close!(pred[0].data(), y[i].data(), abs_tol = 0.1));
+    }
 }
